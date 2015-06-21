bde32ca233f371c3d65677f6a2c213c7a8cad69d
Robert Konrad <robert@ktx-software.com>
2015-06-21 05:43:32 +0200

    Export Android Studio project in Dalvik exporter.

--- DalvikExporter.js
+++ DalvikExporter.js
@@ -1,4 +1,4 @@
-var JavaExporter = require('./JavaExporter.js');
+var KhaExporter = require('./KhaExporter.js');
 var korepath = require('./korepath.js');
 var Converter = require('./Converter.js');
 var Files = require(korepath + 'Files.js');
@@ -6,7 +6,7 @@ var Haxe = require('./Haxe.js');
 var Options = require('./Options.js');
 var Paths = require(korepath + 'Paths.js');
 var exportImage = require('./ImageTool.js');
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
 
 function findIcon(from) {
@@ -15,11 +15,12 @@ function findIcon(from) {
 }
 
 function DalvikExporter(khaDirectory, directory) {
-	JavaExporter.call(this, khaDirectory);
+	KhaExporter.call(this, khaDirectory);
 	this.directory = directory;
-};
+	this.addSourceDirectory(path.join(khaDirectory.toString(), 'Backends/Android'));
+}
 
-DalvikExporter.prototype = Object.create(JavaExporter.prototype);
+DalvikExporter.prototype = Object.create(KhaExporter.prototype);
 DalvikExporter.constructor = DalvikExporter;
 
 DalvikExporter.prototype.sysdir = function () {
@@ -30,156 +31,123 @@ DalvikExporter.prototype.backend = function () {
 	return "Android";
 };
 
-DalvikExporter.prototype.exportEclipseProject = function () {
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), ".classpath")));
-	this.p("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-	this.p("<classpath>");
-		this.p("<classpathentry kind=\"src\" path=\"gen\"/>", 1);
-		this.p("<classpathentry kind=\"src\" path=\"Sources/src\"/>", 1);
-		this.p("<classpathentry kind=\"con\" path=\"com.android.ide.eclipse.adt.ANDROID_FRAMEWORK\"/>", 1);
-		this.p("<classpathentry kind=\"output\" path=\"bin/classes\"/>", 1);
-	this.p("</classpath>");
-	this.closeFile();
-		
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), ".project")));
-	this.p("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-	this.p("<projectDescription>");
-		this.p("<name>" + this.getCurrentDirectoryName(this.directory) + "</name>", 1);
-		this.p("<comment></comment>", 1);
-		this.p("<projects>", 1);
-		this.p("</projects>", 1);
-		this.p("<buildSpec>", 1);
-			this.p("<buildCommand>", 2);
-				this.p("<name>com.android.ide.eclipse.adt.ResourceManagerBuilder</name>", 3);
-				this.p("<arguments>", 3);
-				this.p("</arguments>", 3);
-			this.p("</buildCommand>", 2);
-			this.p("<buildCommand>", 2);
-				this.p("<name>com.android.ide.eclipse.adt.PreCompilerBuilder</name>", 3);
-				this.p("<arguments>", 3);
-				this.p("</arguments>", 3);
-			this.p("</buildCommand>", 2);
-			this.p("<buildCommand>", 2);
-				this.p("<name>org.eclipse.jdt.core.javabuilder</name>", 3);
-				this.p("<arguments>", 3);
-				this.p("</arguments>", 3);
-			this.p("</buildCommand>", 2);
-			this.p("<buildCommand>", 2);
-				this.p("<name>com.android.ide.eclipse.adt.ApkBuilder</name>", 3);
-				this.p("<arguments>", 3);
-				this.p("</arguments>", 3);
-			this.p("</buildCommand>", 2);
-		this.p("</buildSpec>", 1);
-		this.p("<natures>", 1);
-			this.p("<nature>com.android.ide.eclipse.adt.AndroidNature</nature>", 2);
-			this.p("<nature>org.eclipse.jdt.core.javanature</nature>", 2);
-		this.p("</natures>", 1);
-	this.p("</projectDescription>");
-	this.closeFile();
-		
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), "AndroidManifest.xml")));
+DalvikExporter.prototype.exportSolution = function (name, platform, khaDirectory, haxeDirectory, from, callback) {
+	this.createDirectory(this.directory.resolve(this.sysdir()));
+
+	this.writeFile(this.directory.resolve("project-" + this.sysdir() + ".hxproj"));
 	this.p("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-	this.p("<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"kha.android\" android:versionCode=\"1\" android:versionName=\"1.0\">");
-		this.p("<application android:icon=\"@drawable/icon\" android:label=\"@string/app_name\">", 1);
-			this.p("<activity android:label=\"@string/app_name\" android:screenOrientation=\"landscape\" android:name=\"kha.android.Game\" android:theme=\"@android:style/Theme.NoTitleBar.Fullscreen\">", 2);
-				this.p("<intent-filter>", 3);
-					this.p("<action android:name=\"android.intent.action.MAIN\" />", 4);
-					this.p("<category android:name=\"android.intent.category.LAUNCHER\" />", 4);
-				this.p("</intent-filter>", 3);
-			this.p("</activity>", 2);
-		this.p("</application>", 1);
-	this.p("</manifest>");
-	this.closeFile();
-		
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), "proguard.cfg")));
-	this.p("-optimizationpasses 5");
-	this.p("-dontusemixedcaseclassnames");
-	this.p("-dontskipnonpubliclibraryclasses");
-	this.p("-dontpreverify");
-	this.p("-verbose");
-	this.p("-optimizations !code/simplification/arithmetic,!field/*,!class/merging/*");
-	this.p();
-	this.p("-keep public class * extends android.app.Activity");
-	this.p("-keep public class * extends android.app.Application");
-	this.p("-keep public class * extends android.app.Service");
-	this.p("-keep public class * extends android.content.BroadcastReceiver");
-	this.p("-keep public class * extends android.content.ContentProvider");
-	this.p("-keep public class * extends android.app.backup.BackupAgentHelper");
-	this.p("-keep public class * extends android.preference.Preference");
-	this.p("-keep public class com.android.vending.licensing.ILicensingService");
-	this.p();
-	this.p("-keepclasseswithmembernames class * {");
-		this.p("native <methods>;", 1);
-	this.p("}");
-	this.p();
-	this.p("-keepclasseswithmembernames class * {");
-		this.p("public <init>(android.content.Context, android.util.AttributeSet);", 1);
-	this.p("}");
-	this.p();
-	this.p("-keepclasseswithmembernames class * {");
-		this.p("public <init>(android.content.Context, android.util.AttributeSet, int);", 1);
-	this.p("}");
-	this.p();
-	this.p("-keepclassmembers enum * {");
-	this.p("public static **[] values();");
-	this.p("public static ** valueOf(java.lang.String);");
-	this.p("}");
-	this.p();
-	this.p("-keep class * implements android.os.Parcelable {");
-		this.p("public static final android.os.Parcelable$Creator *;", 1);
-	this.p("}");
-	this.closeFile();
-		
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), "project.properties")));
-	this.p("target=android-7");
-	this.closeFile();
-		
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), ".settings")));
-		
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), ".settings", "org.eclipse.jdt.core.prefs")));
-	this.p("#Thu Oct 20 20:02:57 CEST 2011");
-	this.p("eclipse.preferences.version=1");
-	this.p("org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled");
-	this.p("org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.6");
-	this.p("org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve");
-	this.p("org.eclipse.jdt.core.compiler.compliance=1.6");
-	this.p("org.eclipse.jdt.core.compiler.debug.lineNumber=generate");
-	this.p("org.eclipse.jdt.core.compiler.debug.localVariable=generate");
-	this.p("org.eclipse.jdt.core.compiler.debug.sourceFile=generate");
-	this.p("org.eclipse.jdt.core.compiler.problem.assertIdentifier=error");
-	this.p("org.eclipse.jdt.core.compiler.problem.enumIdentifier=error");
-	this.p("org.eclipse.jdt.core.compiler.source=1.6");
+	this.p("<project version=\"2\">");
+	this.p("<!-- Output SWF options -->", 1);
+	this.p("<output>", 1);
+	this.p("<movie outputType=\"Application\" />", 2);
+	this.p("<movie input=\"\" />", 2);
+	this.p('<movie path="' + this.sysdir() + '\\app\\src" />', 2);
+	this.p("<movie fps=\"0\" />", 2);
+	this.p("<movie width=\"0\" />", 2);
+	this.p("<movie height=\"0\" />", 2);
+	this.p("<movie version=\"1\" />", 2);
+	this.p("<movie minorVersion=\"0\" />", 2);
+	this.p("<movie platform=\"Java\" />", 2);
+	this.p("<movie background=\"#FFFFFF\" />", 2);
+	if (Files.isDirectory(haxeDirectory)) this.p('<movie preferredSDK="' + from.resolve('build').relativize(haxeDirectory).toString() + '" />', 2);
+	this.p("</output>", 1);
+	this.p("<!-- Other classes to be compiled into your SWF -->", 1);
+	this.p("<classpaths>", 1);
+	for (var i = 0; i < this.sources.length; ++i) {
+		if (path.isAbsolute(this.sources[i])) {
+			this.p('<class path="' + this.sources[i] + '" />', 2);
+		}
+		else {
+			this.p('<class path="' + from.resolve('build').relativize(from.resolve(this.sources[i])).toString() + '" />', 2);
+		}
+	}
+	this.p("</classpaths>", 1);
+	this.p("<!-- Build options -->", 1);
+	this.p("<build>", 1);
+	this.p("<option directives=\"\" />", 2);
+	this.p("<option flashStrict=\"False\" />", 2);
+	this.p("<option mainClass=\"Main\" />", 2);
+	this.p("<option enabledebug=\"False\" />", 2);
+	this.p("<option additional=\"-D no-compilation&#xA;-java-lib " + from.resolve('build').relativize(haxeDirectory).resolve(Paths.get("hxjava", "hxjava-std.jar")).toString() + "\" />", 2);
+	this.p("</build>", 1);
+	this.p("<!-- haxelib libraries -->", 1);
+	this.p("<haxelib>", 1);
+	this.p("<!-- example: <library name=\"...\" /> -->", 2);
+	this.p("</haxelib>", 1);
+	this.p("<!-- Class files to compile (other referenced classes will automatically be included) -->", 1);
+	this.p("<compileTargets>", 1);
+	this.p("<compile path=\"..\\Sources\\Main.hx\" />", 2);
+	this.p("</compileTargets>", 1);
+	this.p("<!-- Paths to exclude from the Project Explorer tree -->", 1);
+	this.p("<hiddenPaths>", 1);
+	this.p("<!-- example: <hidden path=\"...\" /> -->", 2);
+	this.p("</hiddenPaths>", 1);
+	this.p("<!-- Executed before build -->", 1);
+	this.p("<preBuildCommand />", 1);
+	this.p("<!-- Executed after build -->", 1);
+	this.p("<postBuildCommand alwaysRun=\"False\" />", 1);
+	this.p("<!-- Other project options -->", 1);
+	this.p("<options>", 1);
+	this.p("<option showHiddenPaths=\"False\" />", 2);
+	this.p("<option testMovie=\"OpenDocument\" />", 2);
+	this.p("<option testMovieCommand=\"\" />", 2);
+	this.p("</options>", 1);
+	this.p("<!-- Plugin storage -->", 1);
+	this.p("<storage />", 1);
+	this.p("</project>");
 	this.closeFile();
 
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "res")));
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "res", "drawable-hdpi")));
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "res", "drawable-mdpi")));
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "res", "drawable-ldpi")));
-	
-	exportImage(findIcon(this.directory), this.directory.resolve(Paths.get(this.sysdir(), "res", "drawable-hdpi", "icon.png")), { width: 72, height: 72 });
-	exportImage(findIcon(this.directory), this.directory.resolve(Paths.get(this.sysdir(), "res", "drawable-mdpi", "icon.png")), { width: 48, height: 48 });
-	exportImage(findIcon(this.directory), this.directory.resolve(Paths.get(this.sysdir(), "res", "drawable-ldpi", "icon.png")), { width: 36, height: 36 });
-	
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "res", "layout")));
-	
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), "res", "layout", "main.xml")));
-	this.p("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-	this.p("<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\" android:orientation=\"vertical\" android:layout_width=\"fill_parent\" android:layout_height=\"fill_parent\">");
-		this.p("<TextView android:layout_width=\"fill_parent\" android:layout_height=\"wrap_content\" android:text=\"@string/hello\" />", 1);
-	this.p("</LinearLayout>");
+	this.exportAndroidStudioProject(name);
+
+	this.writeFile(this.directory.resolve("project-" + this.sysdir() + ".hxml"));
+	for (var i = 0; i < this.sources.length; ++i) {
+		if (path.isAbsolute(this.sources[i])) {
+			this.p("-cp " + this.sources[i]);
+		}
+		else {
+			this.p("-cp " + from.resolve('build').relativize(from.resolve(this.sources[i])).toString());
+		}
+	}
+	this.p("-java " + path.join(this.sysdir(), 'app', 'src'));
+	this.p("-main Main");
+	this.p("-D no-compilation");
+	this.p("-java-lib " + from.resolve('build').relativize(haxeDirectory).resolve(Paths.get("hxjava", "hxjava-std.jar")).toString());
 	this.closeFile();
-	
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "res", "values")));
-	
-	this.writeFile(this.directory.resolve(Paths.get(this.sysdir(), "res", "values", "strings.xml")));
-	this.p("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-	this.p("<resources>");
-		this.p("<string name=\"hello\">Hello World!</string>", 1);
-		this.p("<string name=\"app_name\">" + this.getCurrentDirectoryName(this.directory) + "</string>", 1);
-	this.p("</resources>");
-	this.closeFile();
-	
-	this.createDirectory(this.directory.resolve(Paths.get(this.sysdir(), "gen")));
+
+	var options = [];
+	options.push("project-" + this.sysdir() + ".hxml");
+	Haxe.executeHaxe(from, haxeDirectory, options, callback);
+};
+
+DalvikExporter.prototype.exportAndroidStudioProject = function (name) {
+	var indir = path.join(__dirname, 'Data', 'android');
+	var outdir = path.join(this.directory.path, this.sysdir());
+
+	fs.copySync(path.join(indir, 'build.gradle'), path.join(outdir, 'build.gradle'));
+	fs.copySync(path.join(indir, 'gradle.properties'), path.join(outdir, 'gradle.properties'));
+	fs.copySync(path.join(indir, 'gradlew'), path.join(outdir, 'gradlew'));
+	fs.copySync(path.join(indir, 'gradlew.bat'), path.join(outdir, 'gradlew.bat'));
+	fs.copySync(path.join(indir, 'name.iml'), path.join(outdir, name + '.iml'));
+	fs.copySync(path.join(indir, 'settings.gradle'), path.join(outdir, 'settings.gradle'));
+
+	fs.copySync(path.join(indir, 'app', 'app.iml'), path.join(outdir, 'app', 'app.iml'));
+	fs.copySync(path.join(indir, 'app', 'build.gradle'), path.join(outdir, 'app', 'build.gradle'));
+	fs.copySync(path.join(indir, 'app', 'proguard-rules.pro'), path.join(outdir, 'app', 'proguard-rules.pro'));
+	fs.ensureDirSync(path.join(outdir, 'app', 'src'));
+	fs.emptyDirSync(path.join(outdir, 'app', 'src'));
+
+	fs.copySync(path.join(indir, 'gradle', 'wrapper', 'gradle-wrapper.jar'), path.join(outdir, 'gradle', 'wrapper', 'gradle-wrapper.jar'));
+	fs.copySync(path.join(indir, 'gradle', 'wrapper', 'gradle-wrapper.properties'), path.join(outdir, 'gradle', 'wrapper', 'gradle-wrapper.properties'));
+
+	fs.copySync(path.join(indir, 'idea', 'compiler.xml'), path.join(outdir, '.idea', 'compiler.xml'));
+	fs.copySync(path.join(indir, 'idea', 'encodings.xml'), path.join(outdir, '.idea', 'encodings.xml'));
+	fs.copySync(path.join(indir, 'idea', 'gradle.xml'), path.join(outdir, '.idea', 'gradle.xml'));
+	fs.copySync(path.join(indir, 'idea', 'misc.xml'), path.join(outdir, '.idea', 'misc.xml'));
+	fs.copySync(path.join(indir, 'idea', 'modules.xml'), path.join(outdir, '.idea', 'modules.xml'));
+	fs.copySync(path.join(indir, 'idea', 'name'), path.join(outdir, '.idea', '.name'));
+	fs.copySync(path.join(indir, 'idea', 'runConfigurations.xml'), path.join(outdir, '.idea', 'runConfigurations.xml'));
+	fs.copySync(path.join(indir, 'idea', 'vcs.xml'), path.join(outdir, '.idea', 'vcs.xml'));
+	fs.copySync(path.join(indir, 'idea', 'copyright', 'profiles_settings.xml'), path.join(outdir, '.idea', 'copyright', 'profiles_settings.xml'));
 };
 
 DalvikExporter.prototype.copyMusic = function (platform, from, to, encoders, callback) {
