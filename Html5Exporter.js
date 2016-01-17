6952ed10f665b41bb6ace7d2101ba01543a998dd
Robert Konrad <robert@ktx-software.com>
2016-01-17 20:46:14 +0100

    Write electron startup files.

--- Html5Exporter.js
+++ Html5Exporter.js
@@ -57,15 +57,38 @@ class Html5Exporter extends KhaExporter {
 		};
 		HaxeProject(this.directory.toString(), options);
 
-		let index = this.directory.resolve(Paths.get(this.sysdir(), "index.html"));
-		if (!Files.exists(index)) {
-			let protoindex = fs.readFileSync(path.join(__dirname, 'Data', 'html5', 'index.html'), {encoding: 'utf8'});
-			protoindex = protoindex.replaceAll("{Name}", name);
-			protoindex = protoindex.replaceAll("{Width}", this.width);
-			protoindex = protoindex.replaceAll("{Height}", this.height);
-			fs.writeFileSync(index.toString(), protoindex);
+		if (this.sysdir() === 'debug-html5') {
+			let index = this.directory.resolve(Paths.get(this.sysdir(), 'index.html'));
+			if (!Files.exists(index)) {
+				let protoindex = fs.readFileSync(path.join(__dirname, 'Data', 'debug-html5', 'index.html'), {encoding: 'utf8'});
+				protoindex = protoindex.replaceAll('{Name}', name);
+				protoindex = protoindex.replaceAll('{Width}', this.width);
+				protoindex = protoindex.replaceAll('{Height}', this.height);
+				fs.writeFileSync(index.toString(), protoindex);
+			}
+			
+			let pack = this.directory.resolve(Paths.get(this.sysdir(), 'package.json'));
+			let protopackage = fs.readFileSync(path.join(__dirname, 'Data', 'debug-html5', 'package.json'), {encoding: 'utf8'});
+			protopackage = protopackage.replaceAll('{Name}', name);
+			fs.writeFileSync(pack.toString(), protopackage);
+			
+			let electron = this.directory.resolve(Paths.get(this.sysdir(), 'electron.js'));
+			let protoelectron = fs.readFileSync(path.join(__dirname, 'Data', 'debug-html5', 'electron.js'), {encoding: 'utf8'});
+			protoelectron = protoelectron.replaceAll('{Width}', this.width + 20);
+			protoelectron = protoelectron.replaceAll('{Height}', this.height + 20);
+			fs.writeFileSync(electron.toString(), protoelectron);
 		}
-
+		else {		
+			let index = this.directory.resolve(Paths.get(this.sysdir(), 'index.html'));
+			if (!Files.exists(index)) {
+				let protoindex = fs.readFileSync(path.join(__dirname, 'Data', 'html5', 'index.html'), {encoding: 'utf8'});
+				protoindex = protoindex.replaceAll('{Name}', name);
+				protoindex = protoindex.replaceAll('{Width}', this.width);
+				protoindex = protoindex.replaceAll('{Height}', this.height);
+				fs.writeFileSync(index.toString(), protoindex);
+			}
+		}
+		
 		if (Options.compilation) {
 			Haxe.executeHaxe(this.directory, haxeDirectory, ['project-' + this.sysdir() + '.hxml'], callback);
 		}
