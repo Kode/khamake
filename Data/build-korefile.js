ec8d2916c2dbc499023309b81f3fdf2371f68f15
Robert Konrad <robert@ktx-software.com>
2016-04-06 10:27:08 +0200

    Added pch option to cross-compiled C++ files.

--- Data/build-korefile.js
+++ Data/build-korefile.js
@@ -1,5 +1,6 @@
 var project = new Project('HaxeCrossCode');
-project.addFiles('Sources/**.h', 'Sources/**.cpp', 'Sources/**.metal');
+project.addFiles('Sources/**.h', 'Sources/**.cpp', { pch: 'hxcpp.h' });
+project.addFiles('Sources/**.metal');
 project.addExcludes('Sources/src/__main__.cpp');
 project.addIncludeDirs('Sources/include');
 return project;
