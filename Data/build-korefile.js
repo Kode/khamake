526153c98908576463c6cfc38c2a5fa1a00ac5d1
Robert Konrad <robert@ktx-software.com>
2016-04-29 17:38:01 +0200

    Excluded some files from using pch.

--- Data/build-korefile.js
+++ Data/build-korefile.js
@@ -1,5 +1,6 @@
 var project = new Project('HaxeCrossCode');
 project.addFiles('Sources/**.h', 'Sources/**.cpp', { pch: 'hxcpp.h' });
+project.addFiles('Sources/src/__lib__.cpp', 'Sources/src/__boot__.cpp');
 project.addFiles('Sources/**.metal');
 project.addExcludes('Sources/src/__main__.cpp');
 project.addIncludeDirs('Sources/include');
