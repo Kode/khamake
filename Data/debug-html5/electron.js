fe6f26a94643b27d04a803f313dc129c3841132a
Robert Konrad <git2016@robdangero.us>
2016-07-14 08:14:46 +0200

    New window init functionality in debug-html5.

--- Data/debug-html5/electron.js
+++ Data/debug-html5/electron.js
@@ -6,12 +6,17 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
 
+electron.ipcMain.on('asynchronous-message', (event, arg) => {
+	if (arg.width && arg.height) mainWindow.setContentSize(arg.width, arg.height);
+	mainWindow.show();
+});
+
 app.on('window-all-closed', function () {
 	app.quit();
 });
 
 app.on('ready', function () {
-	mainWindow = new BrowserWindow({width: {Width}, height: {Height}, useContentSize: true, autoHideMenuBar: true});
+	mainWindow = new BrowserWindow({width: {Width}, height: {Height}, show: false, useContentSize: true, autoHideMenuBar: true});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 	mainWindow.on('closed', function() {
 		mainWindow = null;
