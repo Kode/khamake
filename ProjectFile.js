dc2c2f0e5055455bd401705da8a5650a9b44ed56
Robert Konrad <git2016@robdangero.us>
2016-07-27 18:08:53 +0200

    Add support for require in khafiles

--- ProjectFile.js
+++ ProjectFile.js
@@ -7,6 +7,6 @@ const Project = require('./Project.js');
 module.exports = function (from, projectfile) {
 	let file = fs.readFileSync(from.resolve(projectfile).toString(), { encoding: 'utf8' });
 	Project.scriptdir = from.toString();
-	let project = new Function(['Project'], file)(Project);
+	let project = new Function(['Project', 'require'], file)(Project, require);
 	return project;
 };
