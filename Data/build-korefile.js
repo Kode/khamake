141ec54bfa0f5cdacc1587ca39e23ee8462950b1
Robert Konrad <git2016@robdangero.us>
2017-02-03 21:42:04 +0100

    Fix Haxe resources compilation in Visual Studio
    Fixes Kode/Kha#504

--- Data/build-korefile.js
+++ Data/build-korefile.js
@@ -1,5 +1,6 @@
 let project = new Project('HaxeCrossCode', __dirname);
 project.addFiles('Sources/**.h', 'Sources/**.cpp', { pch: 'hxcpp.h' });
+project.addFiles('Sources/src/resources/**.cpp');
 project.addFiles('Sources/src/__lib__.cpp', 'Sources/src/__boot__.cpp');
 project.addFiles('Sources/**.metal');
 project.addExcludes('Sources/src/__main__.cpp');
