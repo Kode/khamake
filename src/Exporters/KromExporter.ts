5dbc9f69935e24e6b8a46ae889d9139e245a3f75
Lubos Lenco <lubos.lenco@gmail.com>
2017-05-27 10:31:22 +0200

    Clean up

--- src/Exporters/KromExporter.ts
+++ src/Exporters/KromExporter.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import * as os from 'os';
 import {KhaExporter} from './KhaExporter';
 import {convert} from '../Converter';
 import {executeHaxe} from '../Haxe';
