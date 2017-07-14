f0883f19302d45594524c959391b5cdf50bada45
David Klein <david.klein@psmtec.de>
2017-07-14 10:18:37 +0200

    fallback to Main in case options.main doesn't exist

--- src/HaxeProject.ts
+++ src/HaxeProject.ts
@@ -142,7 +142,7 @@ function hxml(projectdir: string, options: any) {
 	for (let param of options.parameters) {
 		data += param + '\n';
 	}
-	data += '-main ' + options.main + '\n';
+	data += '-main ' + options.main !== null ? options.main : 'Main' + '\n';
 	fs.outputFileSync(path.join(projectdir, 'project-' + options.system + '.hxml'), data);
 }
 
