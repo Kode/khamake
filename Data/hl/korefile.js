3fe3725434b9b08afd4d8a398304fd03bc8cdc8e
unknown <lubos.lenco@gmail.com>
2018-03-11 10:17:02 +0100

    Cleanup

--- Data/hl/korefile.js
+++ Data/hl/korefile.js
@@ -1,8 +1,4 @@
 let project = new Project('HaxeCrossCode', __dirname);
-project.addFiles('_std/**');
-project.addFiles('haxe/**');
-project.addFiles('hl/**');
-project.addFiles('kha/**');
 project.addFiles('kore_sources.c');
 project.addIncludeDirs('.');
 resolve(project);
