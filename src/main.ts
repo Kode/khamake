ecb7619c3cd43d962aabcf236d61968f12557188
Zicklag <zicklag@katharostech.com>
2019-01-22 12:26:18 -0600

    Fix korefile.js Subproject Path
    Fixes #178

--- src/main.ts
+++ src/main.ts
@@ -81,9 +81,9 @@ function createKorefile(name: string, exporter: KhaExporter, options: any, targe
 		out += 'project.targetOptions = ' + JSON.stringify(koreTargetOptions) + ';\n';
 	}
 
-	out += 'project.setDebugDir(\'' + path.relative(options.from, path.join(options.to, exporter.sysdir())).replace(/\\/g, '/') + '\');\n';
+	out += 'project.setDebugDir(\'' + path.relative(options.from, exporter.sysdir()).replace(/\\/g, '/') + '\');\n';
 
-	let buildpath = path.relative(options.from, path.join(options.to, exporter.sysdir() + '-build')).replace(/\\/g, '/');
+	let buildpath = path.relative(options.from, exporter.sysdir() + '-build').replace(/\\/g, '/');
 	if (buildpath.startsWith('..')) buildpath = path.resolve(path.join(options.from.toString(), buildpath));
 	out += 'await project.addProject(\'' + buildpath.replace(/\\/g, '/') + '\');\n';
 	if (korehl) out += 'await project.addProject(\'' + path.join(options.kha, 'Backends', 'KoreHL').replace(/\\/g, '/') + '\');\n';
@@ -157,7 +157,7 @@ async function exportProjectFiles(name: string, resourceDir: string, options: Op
 			{
 				from: options.from,
 				to: buildDir,
-				korefile: 'build/korefile.js',
+				korefile: `${buildDir}/korefile.js`,
 				target: koreplatform(options.target),
 				graphics: options.graphics,
 				audio: options.audio,
