42aedd8dd59c5c533c40e580004c9303ee6ee6be
Robert Konrad <git2016@robdangero.us>
2020-12-05 17:02:31 +0100

    Remove a dot

--- out/main.js
+++ out/main.js
@@ -702,7 +702,7 @@ async function run(options, loglog) {
         console.log();
         console.log('The Unity target hasn\'t been updated for years.\n'
             + 'Rob thinks it is not needed anymore now that\n'
-            + 'proper console backends exist but please tell.\n'
+            + 'proper console backends exist but please tell\n'
             + 'Rob if you think he is wrong about that.');
         console.log();
     }
--- src/main.ts
+++ src/main.ts
@@ -788,7 +788,7 @@ export async function run(options: Options, loglog: any): Promise<string> {
 		console.log(
 			'The Unity target hasn\'t been updated for years.\n'
 			+ 'Rob thinks it is not needed anymore now that\n'
-			+ 'proper console backends exist but please tell.\n'
+			+ 'proper console backends exist but please tell\n'
 			+ 'Rob if you think he is wrong about that.'
 		);
 		console.log();
