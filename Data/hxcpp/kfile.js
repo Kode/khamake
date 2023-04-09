fcb5327807b00b6091282034133145aa97a1a400
Robert Konrad <git2016@robdangero.us>
2023-04-09 22:58:56 +0200

    Remove superfluous metal-includes

--- Data/hxcpp/kfile.js
+++ Data/hxcpp/kfile.js
@@ -7,7 +7,6 @@ else {
 }
 project.addFiles('Sources/src/resources/**.cpp');
 project.addFiles('Sources/src/__lib__.cpp', 'Sources/src/__boot__.cpp');
-project.addFiles('Sources/**.metal');
 project.addExcludes('Sources/src/__main__.cpp');
 project.addIncludeDirs('Sources/include');
 //project.addDefine('HX_SMART_STRINGS');
