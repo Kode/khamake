65f368796809b074fa6fabe96aefbad8e180f532
Robert Konrad <git2016@robdangero.us>
2023-04-09 16:08:52 +0200

    Disable HX_SMART_STRINGS for now

--- Data/hxcpp/kfile.js
+++ Data/hxcpp/kfile.js
@@ -10,5 +10,5 @@ project.addFiles('Sources/src/__lib__.cpp', 'Sources/src/__boot__.cpp');
 project.addFiles('Sources/**.metal');
 project.addExcludes('Sources/src/__main__.cpp');
 project.addIncludeDirs('Sources/include');
-project.addDefine('HX_SMART_STRINGS');
+//project.addDefine('HX_SMART_STRINGS');
 resolve(project);
